f33d304378e5d10fd35c24ee78f91c2b7bd4f4ad
Jason Halsey <jasonhalsey@01260-MBP15R.local>
2016-10-31 10:03:40 -0700

    WIP: Working without controller as

--- js/app.js
+++ js/app.js
@@ -1,14 +1,35 @@
-var app = angular.module('waitwait', [])
-  app.controller('meal_controller', function(){
-    var vm = this;
-    vm.title = "Meal Controller";
-    vm.value = meal.myform.base_meal_price;
-  });
-  app.controller('charges_controller', function(){
-    var vm = this;
-    vm.title = "Charges Controller";
-  });
-  app.controller('earnings_controller', function(){
-    var vm = this;
-    vm.title = "Earnings Controller";
-  });
+var app = angular.module('plunker', []);
+
+app.controller('MainController', function() {
+  this.totalEarnings = {
+    totalTip: 0,
+    mealCount: 0,
+    averageTipPerMeal: 0
+  }
+  
+  this.customerCharges = {
+    subtotal: 0,
+    tip: 0,
+    total: 0
+  }
+  
+  this.submit = function() {
+    // this.updateCustomerCharges()
+    this.customerCharges.subtotal = ((this.myform.taxRate / 100) * this.myform.baseMealPrice) + this.myform.baseMealPrice
+    this.customerCharges.tip = ((this.myform.tipPercentage / 100) * this.myform.baseMealPrice)
+    this.customerCharges.total = this.customerCharges.subtotal + this.customerCharges.tip
+    
+    // this.updateTotalEarnigs()
+    this.totalEarnings.totalTip = this.totalEarnings.totalTip + this.customerCharges.tip
+    this.totalEarnings.mealCount = this.totalEarnings.mealCount + 1 
+    this.totalEarnings.averageTipPerMeal = this.totalEarnings.totalTip / this.totalEarnings.mealCount
+  }
+
+  this.clear = function clear(){
+    this.myform = this.totalEarnings
+  }
+  this.submit = function submit(){
+    this.myform = this.totalEarnings
+  }
+
+});
